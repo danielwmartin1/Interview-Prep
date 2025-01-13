ae7f9e0107953003a3e9a0834160a54afa40e78e
danielwmartin1 <danielwmartin1@gmail.com>
2025-01-13 01:57:42 -0500

    feat: implement maxArea function to calculate maximum area from input heights

---
+++ maxArea.js
@@ -0,0 +1,35 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const maxArea = function (height) {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    let width = right - left;
+    let currentArea = Math.min(height[left], height[right]) * width;
+    console.log(`left: ${left}, right: ${right}, width: ${width}, currentArea: ${currentArea}`);
+    maxArea = Math.max(maxArea, currentArea);
+    console.log(`Updated maxArea: ${maxArea}`);
+    if (height[left] <= height[right]) {
+      left++;
+      console.log(`Incremented left to: ${left}`);
+    } else {
+      right--;
+      console.log(`Decremented right to: ${right}`);
+    }
+  }
+  return maxArea;
+};
+
+rl.question('Enter heights separated by commas: ', (answer) => {
+  const height = answer.split(',').map(Number);
+  console.log('Input heights:', height);
+  console.log('Maximum area:', maxArea(height));
+  rl.close();
+});
